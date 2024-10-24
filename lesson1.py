8c096effe5d6654363d257ce7a49b71b8c01acf6
Sukotoo <voropaep@wlhs.wlwv.k12.or.us>
2024-10-24 08:37:49 -0700

    reset

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,22 @@
-'''
-Name:
+"""
+Name: Petr V.
 Date: 10/22/24
 Description: Unit 4 Lesson 1 - while loops
+"""
+# while loops run while a condition is true
+# we usually don't know how many times, this
+# scenarios is when they are most often used
 
-'''
+"""
+boolean
 
+while boolean == true:
+    code
+    update/break
+"""
+
+num = 10
+while num >= 0:
+    print(num, end=" ")
+    num -= 1
+print("Lift off!")
